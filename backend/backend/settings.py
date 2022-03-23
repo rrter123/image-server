73d20b378ebd42aa69c1c749d714dbd63aa8adf2
Alicja Gustowska <alicja.gustowska@gmail.com>
2022-03-23 11:03:00 +0100

    Initial models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'images',
 ]
 
 MIDDLEWARE = [
--- backend/images/models.py
+++ backend/images/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class UploadedImage(models.Model):
+    name = models.TextField(blank=True, default='')
+    user = models.ForeignKey(User, related_name='images', on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='images/')
